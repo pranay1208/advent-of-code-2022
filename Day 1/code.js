e9b9fe9e55d8931079fa9250ab55637330ad5f80
Pranay Agarwal <47204059+pranay1208@users.noreply.github.com>
2022-12-02 23:14:48 +0800

    Update code.js

--- Day 1/code.js
+++ Day 1/code.js
@@ -3,11 +3,11 @@ const input = "<input from file>".trim().split('\n\n')
 // Part 1
 sums = input.map(i => i.trim().split('\n')).reduce((sum, val) => sum + parseInt(val), 0)
 max = sums.reduce((max, val) => max > val ? max : val, 0)
-
+console.log("Answer to part 1 -> max =", max)
 // Answer to part 1 -> max = 67450
 
 // Part 2
 sums.sort((a,b) => b - a)
 maxTop3 = sums[0] + sums[1] + sums[2]
-
+console.log("Answer to part 2 -> maxTop3 =", maxTop3")
 // Answer to part 2 -> maxTop3 = 199357
